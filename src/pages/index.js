6ee7b491e6079bd22981d885eb38af1d495a5e1b
Lars Nyström <nystrom.lasse@gmail.com>
2020-08-29 12:41:46 +0200

    Add header and fix some styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,13 +6,83 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+import headerImage from "../images/skydive_2304x1032.webp"
 import travelImage from "../images/img_8074_318x180.webp"
 import workImage from "../images/kryssning_318x180.webp"
 import spareTimeImage from "../images/loro_parque_318x180.webp"
+import facebookIcon from "../images/Facebook.webp"
+import githubIcon from "../images/Github.webp"
+import instagramIcon from "../images/Instagram.webp"
+import linkedinIcon from "../images/Linkedin.webp"
+import twitterIcon from "../images/Twitter.webp"
 
 const IndexPage = () => (
   <Layout pageInfo={{ pageName: "index" }}>
     <SEO title="Home" keywords={[`gatsby`, `react`, `bootstrap`]} />
+    <Container className="header">
+      <div className="header header-home">
+        <div className="banner-text text-center">
+          <h1>I'm Lars Nyström</h1>
+          <p>
+            Systems and their data, keeping 'em all together.
+            <br /> Love to travel to places with lots of sun or lots of snow.
+            <br />
+            <b>
+              Skinnskatteberg {"->"} Gothenburg {"->"} Skövde
+            </b>
+          </p>
+          <div class="socialicons">
+            <ul>
+              <li>
+                <a
+                  href="https://www.linkedin.com/in/larsnystrom"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src={linkedinIcon} alt="LinkedIn" />
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://twitter.com/433eros"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src={twitterIcon} alt="Twitter" />
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.instagram.com/littleandi"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src={instagramIcon} alt="Instagram" />
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.facebook.com/lasse77"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src={facebookIcon} alt="Facebook" />
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://github.com/LittleAndi"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src={githubIcon} alt="Github" />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </Container>
     <Container className="text-center">
       <Row>
         <Col>
